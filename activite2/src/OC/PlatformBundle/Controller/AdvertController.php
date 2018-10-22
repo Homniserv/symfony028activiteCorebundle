00c377940691fe0f4e868c6b7c9bbd5ba2495517
moueza <mouezapeter@gmail.com>
2018-10-22 02:53:10 +0200

    variables webmaster

---
+++ activite2/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,7 @@
+<?php
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+// Dans l'action indexAction() :
+return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+  'listAdverts' => array()
+));
