6812d705ef99a59387ec11f79864de2ce8a89aa9
moueza <mouezapeter@gmail.com>
2018-10-25 10:04:07 +0200

    Resolves #7 platform/1 OK

--- activite2/src/OC/PlatformBundle/Controller/AdvertController.php
+++ activite2/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -32,7 +32,8 @@ class AdvertController extends Controller
 								    // Tout l'intérêt est ici : le contrôleur passe
 								    // les variables nécessaires au template !
 								    'listAdverts' => $listAdverts
-								    ));
+								    )
+			 );
   }
 
 
@@ -67,8 +68,41 @@ class AdvertController extends Controller
 
     // Ici, on récupérera la liste des annonces, puis on la passera au template
 
+    // Notre liste d'annonce en dur
+    $listAdverts = array(
+			 array(
+			       'title'   => 'Recherche développpeur Symfony',
+			       'id'      => 1,
+			       'author'  => 'Alexandre',
+			       'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+			       'date'    => new \Datetime()),
+			 array(
+			       'title'   => 'Mission de webmaster',
+			       'id'      => 2,
+			       'author'  => 'Hugo',
+			       'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+			       'date'    => new \Datetime()),
+			 array(
+			       'title'   => 'Offre de stage webdesigner',
+			       'id'      => 3,
+			       'author'  => 'Mathieu',
+			       'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+			       'date'    => new \Datetime())
+			 );
+
+
+
+    
     // Mais pour l'instant, on ne fait qu'appeler le template
-    return $this->render('@OCPlatform/Advert/index.html.twig');
+    // return $this->render('@OCPlatform/Advert/index.html.twig');
+
+
+
+
+    // Et modifiez le 2nd argument pour injecter notre liste
+    return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+									  'listAdverts' => $listAdverts
+									  ));
   }
 
 
